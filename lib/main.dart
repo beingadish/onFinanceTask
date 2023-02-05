b4729eaf6337a252f2643d2e3c8eeb15834aaf77
Aadarsh Pandey <aadarshpandey.pandey@gmail.com>
2023-02-05 11:42:36 +0530

    App Bar UI Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:on_finance_task/model/data_model.dart';
 
 void main() {
   runApp(const MainApp());
@@ -19,7 +21,66 @@ class _MainAppState extends State<MainApp> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          toolbarHeight: 70.0,
+          title: const Text("Polygon"),
+          leading: const Icon(Icons.arrow_back_ios_new),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white12,
+                ),
+                child: const Icon(Icons.bookmark_border_rounded),
+              ),
+            )
+          ],
           backgroundColor: Colors.black,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Divider(
+                height: 0.1,
+                indent: 2.0,
+                color: Colors.white,
+              ),
+              Stack(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                    child: SfCartesianChart(
+                      margin: const EdgeInsets.all(0),
+                      primaryXAxis: DateTimeAxis(
+                        minimum: DateTime(2005),
+                        maximum: DateTime.now(),
+                      ),
+                      primaryYAxis: NumericAxis(
+                        borderColor: Colors.amber,
+                        minimum: 20,
+                        maximum: 100,
+                      ),
+                      zoomPanBehavior: ZoomPanBehavior(enablePinching: true),
+                      backgroundColor: const Color.fromRGBO(17, 17, 21, 0.95),
+                    ),
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(30.0),
+                          bottomRight: Radius.circular(30.0),
+                        ),),
+                    height: 120,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/model/data_model.dart
@@ -0,0 +1,5 @@
+class StockData{
+  StockData(this.time,this.value);
+  DateTime time;
+  int value;
+}
