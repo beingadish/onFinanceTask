7fa2e72aefa093bef4e03afcb2664e97c60af7f0
Aadarsh Pandey <aadarshpandey.pandey@gmail.com>
2023-02-05 09:16:09 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/onFinance/on_finance_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.onFinance.on_finance_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+        ),
+      ),
+    );
+  }
+}
